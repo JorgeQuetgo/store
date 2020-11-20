de24a3198f763905112ff9ee73551aa977cb252b
Jorge <jrgjit@gmail.com>
2020-11-19 18:53:48 -0600

    Revert "test"
    This reverts commit aab2ab7dc7f9812d5295423a302c78fe7bf0f353.

--- db/mongodb.js
+++ db/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connection.on('open', () => console.log('db connected'))
 
 async function connectDB({ host, port, dbName }){
-  const uri = host
+  const uri = `mongodb://${host}:${port}/${dbName}`
   await mongoose.connect(uri, {useNewUrlParser: true})
 }
 
